aa79e1e5ac46ae533f561e8aa3a88c799a6277c1
Aleksey Budanov <31888451+AlekseyMinsk@users.noreply.github.com>
2017-09-28 19:25:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // my solution
+  var strArr = str.split('');
+   for(var i = 0; (i + 1) < strArr.length; i++) {
+     var currBrackets = [strArr[i], strArr[i + 1]];
+     for(var j = 0; j < bracketsConfig.length; j++) {
+ 			if(currBrackets[0] == bracketsConfig[j][0]
+ 				&& currBrackets[1] == bracketsConfig[j][1]) {
+ 				strArr.splice(i, 2);
+ 				i = -1;
+ 				break;
+ 			}
+ 		}
+ 	}
+ 	return !strArr.length;
 }
